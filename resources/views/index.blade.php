41229f3f1107498c14ff3ad5e64582a104770c68
GhDj <contact@cazus-dev.com>
2017-06-28 14:51:04 +0100

    Timeline @ /test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,79 @@
 @extends('layout')
+
+@section('content')
+
+
+    <div id="myTimeline" class="container">
+    </div>
+
+@endsection
+
+@section('footer-script')
+
+    <script src="{{ asset('js/jquery-albe-timeline-2.0.1.min.js') }}"></script>
+
+@endsection
+
+@section('script')
+    //Json Object
+    var data = [
+    @foreach(\App\Reunion::all() as $r)
+
+        {
+        time: '{{ \Carbon\Carbon::createFromFormat('d m Y',$r->dateReunion)->format('Y-m-d') }}',
+        body: [{
+        tag: 'h3',
+        content: '{{ \Carbon\Carbon::createFromFormat('d m Y',$r->dateReunion)->format('Y-m-d')  }}',
+        attr: {
+        cssclass: 'group-title'
+        }
+        },
+        {
+        tag: 'span',
+        content: 'Réunion à {{ $r->Emplacement }}',
+        attr: {
+        cssclass: 'group-sub-title'
+        }
+        },
+        {
+        tag: 'p',
+        content: '{{ $r->designation }}Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa, suscipit eu elit urna in urna, gravida wisi aenean eros massa, cursus quisque leo quisque dui.'
+        }]
+        },
+
+    @endforeach
+
+    @foreach(\App\Projet::all() as $p)
+
+        {
+        time: '{{ \Carbon\Carbon::createFromFormat('d m Y',$p->date_debut)->format('Y-m-d') }}',
+        body: [{
+        tag: 'h3',
+        content: '{{ \Carbon\Carbon::createFromFormat('d m Y',$p->date_debut)->format('Y-m-d')  }}',
+        attr: {
+        cssclass: 'group-title'
+        }
+        },
+        {
+        tag: 'span',
+        content: 'Projet à {{ $p->lieu}}',
+        attr: {
+        cssclass: 'group-sub-title'
+        }
+        },
+        {
+        tag: 'p',
+        content: '{{ $p->description }}Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa, suscipit eu elit urna in urna, gravida wisi aenean eros massa, cursus quisque leo quisque dui.'
+        }]
+        },
+
+    @endforeach
+
+    ];
+
+    $(document).ready(function () {
+
+    $("#myTimeline").albeTimeline(data);
+
+    });
+@endsection
